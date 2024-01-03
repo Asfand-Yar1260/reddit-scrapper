32b57f6256ef917f684d5c137440a312298fdb2e
Asfand Yar <asfand1260@gmail.com>
2024-01-03 20:46:50 +0500

    sorting the list of posts based on 'Engagement' and creating a new File

---
+++ sort.js
@@ -0,0 +1,37 @@
+//importing dependencies
+const fs = require("fs/promises");
+
+async function sort() {
+  try {
+    //checking if the file exists
+    const currentDate = new Date()
+      .toISOString()
+      .replace(/:/g, "-")
+      .slice(0, -14);
+    const filePath = `jsons/List_${currentDate}.json`;
+
+    //reading the file
+    let unsortedData = [];
+    const fileContent = await fs.readFile(filePath, "utf-8");
+    unsortedData = JSON.parse(fileContent);
+    /* const list = require(`jsons/List_${currentDate}.json`); */
+
+    //sorting data
+    let sortedData = unsortedData.sort((a, b) => {
+      if (parseInt(a.Post.Engagement) > parseInt(b.Post.Engagement)) {
+        return -1;
+      }
+    });
+
+    //creating new file for sorted data
+    const newFilePath = `jsons/Sorted_List_${currentDate}.json`;
+    await fs.writeFile(newFilePath, JSON.stringify(sortedData, null, "\t"));
+    console.log("Sorting Complete.");
+  } catch (error) {
+    console.log("Error during sort.");
+    console.error(error);
+  }
+}
+
+sort();
+/* module.exports = { sort }; */
